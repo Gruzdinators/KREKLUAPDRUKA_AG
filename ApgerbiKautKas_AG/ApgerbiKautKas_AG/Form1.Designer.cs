87e651d1679eead165c5bc5bcfd725526ef0b82c
Artis Gruzdiņš <ag2561@gymn.local>
2024-02-14 10:13:33 +0200

    kaut kas jauns

--- ApgerbiKautKas_AG/ApgerbiKautKas_AG/Form1.Designer.cs
+++ ApgerbiKautKas_AG/ApgerbiKautKas_AG/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.KANTE = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // KANTE
+            // 
+            this.KANTE.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.KANTE.Location = new System.Drawing.Point(12, 12);
+            this.KANTE.Name = "KANTE";
+            this.KANTE.Size = new System.Drawing.Size(743, 350);
+            this.KANTE.TabIndex = 1;
+            this.KANTE.Text = "KANTE";
+            this.KANTE.UseVisualStyleBackColor = false;
+            this.KANTE.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(767, 374);
+            this.Controls.Add(this.KANTE);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button KANTE;
     }
 }
 
--- ApgerbiKautKas_AG/ApgerbiKautKas_AG/Form1.cs
+++ ApgerbiKautKas_AG/ApgerbiKautKas_AG/Form1.cs
@@ -20,5 +20,20 @@ namespace ApgerbiKautKas_AG
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
                     }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
